1c43aadd3fa3b1e6132feac05b12bc2d1afeae40
mbernardes19 <bernardes.matheus@outlook.com>
2020-12-05 10:42:32 -0300

    Adds Storage Service

---
+++ src/service/StorageService.ts
@@ -0,0 +1,26 @@
+import MongoClient from 'mongodb';
+import { OperationResult } from '../model/SignalRunner';
+
+export default class StorageService {
+    private _db: MongoClient.Db;
+
+    async connectToDb() {
+        const client = await MongoClient.connect(process.env.MONGO_CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
+        this._db = client.db('bot01');
+    }
+
+    async storeOperationResult(operationResult: OperationResult) {
+        const collection = this._db.collection('signal_results')
+        await collection.save(operationResult);
+    }
+
+    async getAllOperationResults() {
+        const collection = this._db.collection('signal_results')
+        return await collection.find();
+    }
+
+    async clearAllOperationResults() {
+        const collection = this._db.collection('signal_results')
+        await collection.deleteMany({});
+    }
+}
